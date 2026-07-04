134866112095a5bdd3ab6f1f0300c3f32f874687
Jackson Brough <jacksontbrough@gmail.com>
2026-07-03 19:31:15 -0600

    Tomorrow, they say, is Independence Day

---
+++ README.md
@@ -0,0 +1,12 @@
+# agda-mcp
+
+MCP server for Agda.
+
+Prior art:
+
+- https://github.com/faezs/agda-mcp
+- https://github.com/DreamLinuxer/agda-mcp
+
+Haskell MCP library:
+
+- https://github.com/DPella/mcp
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Agda MCP";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packageName = "agda-mcp";
+        package = pkgs.haskellPackages.callCabal2nix packageName ./. { };
+      in
+      {
+        packages = {
+          default = package;
+          ${packageName} = package;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${package}/bin/${packageName}";
+          meta.description = "Run ${packageName}";
+        };
+
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = _: [ package ];
+
+          buildInputs = [
+            pkgs.cabal-install
+            pkgs.haskell-language-server
+            pkgs.fourmolu
+            pkgs.hlint
+            pkgs.ghcid
+          ];
+        };
+      }
+    );
+}
