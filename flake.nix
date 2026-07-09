1692437df87f475adf6265d9a07444330693f38d
Jackson Brough <jacksontbrough@gmail.com>
2026-07-08 22:32:25 -0600

    Use mcp 0.3.2.0

--- flake.nix
+++ flake.nix
@@ -12,8 +12,19 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        mcpSrc = pkgs.fetchFromGitHub {
+          owner = "dpella";
+          repo = "mcp";
+          rev = "151286f7c457d806c57801ba2bb2d2e614ee8e55";
+          hash = "sha256-RGUh1FiLX4hLwzMR2U/LLInYLzqVMAwgOZy2CD6vwns=";
+        };
+        haskellPackages = pkgs.haskellPackages.override {
+          overrides = self: _: {
+            mcp = self.callCabal2nix "mcp" (mcpSrc + "/mcp-server") { };
+          };
+        };
         packageName = "agda-mcp";
-        package = pkgs.haskellPackages.callCabal2nix packageName ./. { };
+        package = haskellPackages.callCabal2nix packageName ./. { };
       in
       {
         packages = {
@@ -27,7 +38,7 @@
           meta.description = "Run ${packageName}";
         };
 
-        devShells.default = pkgs.haskellPackages.shellFor {
+        devShells.default = haskellPackages.shellFor {
           packages = _: [ package ];
 
           buildInputs = [
