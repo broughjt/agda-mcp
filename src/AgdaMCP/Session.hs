e32893baed6b783f16a94d0df6e1785d979c71c2
Jackson Brough <jacksontbrough@gmail.com>
2026-07-12 13:07:53 -0600

    Small wording changes

--- src/AgdaMCP/Session.hs
+++ src/AgdaMCP/Session.hs
@@ -53,9 +53,9 @@ data ProtocolViolation a = ProtocolViolation
   }
   deriving (Foldable, Functor, Show, Traversable)
 
--- A `ProtocolViolation` is a always a bug in agda-mcp. We shouldn't treat them
--- as runtime recoverable--if we encounter one we should not attempt to recover,
--- but instead die loudly with good debugging information.
+-- A `ProtocolViolation` is always a bug in agda-mcp. If we encounter one we
+-- should not attempt to recover, but instead die loudly with good debugging
+-- information.
 instance Exception (ProtocolViolation Value)
 
 -- An Agda session as a value, consisting of the type-checker state paired with
@@ -66,14 +66,14 @@ instance Exception (ProtocolViolation Value)
 -- It turns out all you need to run Agda commands (in the TCM monad) is
 -- `TCState` and `CommandState`. Once you have that, you can run Agda commands
 -- without needing channels, locks, or threading (which is what I was doing
--- before I understand this).
+-- before I understood this).
 data Session = Session TCState CommandState
 
 type SessionM = StateT Session IO
 
 newSession :: IO Session
 newSession = do
-  -- The queue is a inert--nothing ever uses it. We just need to pass it because
+  -- The queue is inert--nothing ever uses it. We just need to pass it because
   -- `CommandState` has a `commandQueue` field which needs to be
   -- initialized. Normally, Agda runs in a separate thread and receives commands
   -- over a channel, but we deliberately avoid that here.
