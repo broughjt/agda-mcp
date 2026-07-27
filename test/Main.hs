274519a1c829d782331f176a3d29c9b4d6d5203c
Jackson Brough <jacksontbrough@gmail.com>
2026-07-27 17:18:46 -0600

    Move to one file for interaction layer tests

--- test/Main.hs
+++ test/Main.hs
@@ -2,15 +2,8 @@ module Main (main) where
 
 import Test.Tasty (defaultMain, testGroup)
 
-import Test.Interaction.Load qualified as Load
+import Test.Interaction qualified as Interaction
 
 main :: IO ()
 main =
-  defaultMain $
-    testGroup
-      "agda-mcp"
-      [ testGroup
-          "interaction"
-          [ Load.tests
-          ]
-      ]
+  defaultMain $ testGroup "agda-mcp" [Interaction.tests]
